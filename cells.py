83ef02a01c4749dc18d071aebf619d5b30a29558
Gennady Kushnir <genkush@rujel.net>
2020-12-09 18:17:26 +0300

    Life

--- cells.py
+++ cells.py
@@ -48,32 +48,53 @@ class Board:
         x, y = key
         self.board[x][y] = value
 
+    def neighbours(self, y, x):
+        rows = self.board[max(0, y-1):y+2]
+        total = 0
+        for row in rows:
+            cells = row[max(0, x-1):x+2]
+            total += sum(cells)
+        return total
+
+    def next_generation(self):
+        newgen = [[0] * self.width for _ in range(self.height)]
+        for y in range(self.height):
+            for x in range(self.width):
+                total = self.neighbours(x,y)
+                if total == 3 or (self[x,y] and total == 4):
+                    newgen[x][y] = True
+        self.board = newgen
 
 pygame.init()
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 
 
-board = Board(4, 3)
-board.set_view(100, 100, 50)
+board = Board(10, 10)
+# board.set_view(100, 100, 50)
 running = True
+mouse = (0, 0)
+clock = pygame.time.Clock()
 while running:
-    screen.fill((0, 0, 0))
-    board.render(screen)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEMOTION:
-            pygame.draw.circle(screen,(255,0,0),event.pos,10)
+            mouse = event.pos
         if event.type == pygame.MOUSEBUTTONDOWN:
             try:
                 cell = board.get_cell(event.pos)
+                # print(board.neighbours(*cell))
                 board[cell] = not board[cell]
             except IndexError:
                 print("Мимо!")
+        if event.type == pygame.KEYDOWN:
+            board.next_generation()
 
+    screen.fill((0, 0, 0))
+    board.render(screen)
+    pygame.draw.circle(screen, (255, 0, 0), mouse, 5)
+    clock.tick(30)
     pygame.display.flip()
-
 
 pygame.quit()
